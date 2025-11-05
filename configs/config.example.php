9888f44f2212de202cf4020f16266b9eff2fb83c
zafki3l <phamdinhthai2005@gmail.com>
2025-11-05 21:51:59 +0700

    add gitignore

---
+++ configs/config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+$rootFolder = basename(dirname(__DIR__));
+define('PROJECT_NAME', $rootFolder);
+define('BASE_PATH', dirname(__DIR__));
+define('VIEW_PATH', BASE_PATH . '/app/Views/');
+define('ROOT_PATH', __DIR__);
+
+define('DB_SERVER', 'YOUR_SERVER');
+define('DB_USER', 'YOUR_USER');
+define('DB_PASSWORD', 'YOUR_PASSWORD');
+define('DB_DATABASE', 'YOUR_DB');
